72de66fa4a74a2fc7bd2372be928e95ec8a9c49c
Am Laher <am@movio.co>
2017-12-28 07:36:08 +1300

    kcgp: better templates, some fixes

--- kcgp/main.go
+++ kcgp/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"os"
 
@@ -10,73 +11,64 @@ import (
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/clientcmd/api"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func main() {
+	log.SetFlags(0)
 	var (
-		fs      = flag.NewFlagSet("kcv", flag.ExitOnError)
-		verbose = fs.Bool("v", false, "verbose")
-		//kubeconfig *string
+		fs            = flag.NewFlagSet("kcgp", flag.ExitOnError)
+		verbose       = fs.Bool("v", false, "verbose")
+		labelSelector = fs.String("l", "", "label selector")
+		format        = fs.String("f", "{{.Name}}{{t}}{{.Status.Phase}}{{nl}}", "Representation of each pod")
 	)
-	/*
-		if home := homeDir(); home != "" {
-			kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-		} else {
-			kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-		}
-	*/
 	contexts, args := kc.Contexts(os.Args[1:])
+	fs.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of kcgp:\n")
+		fmt.Fprintf(os.Stderr, " kcgp [contexts] [options]\n")
+		fmt.Fprintf(os.Stderr, " [contexts] is a comma-delimited list of contexts, as defined in your kubernetes config.\n")
+		fmt.Fprintf(os.Stderr, " [options] defined as follows:\n")
+		fs.PrintDefaults()
+		fmt.Fprintf(os.Stderr, " For field list see API docs for your version.\n e.g. https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.9/#pod-v1-core\n")
+	}
 	fs.Parse(args)
 
-	// use the current context in kubeconfig
-	//config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	rules := clientcmd.NewDefaultClientConfigLoadingRules()
 	kconfig, err := rules.Load()
+	if err != nil {
 
-	if err != nil {
-		panic(err.Error())
-	}
-	overrides := &clientcmd.ConfigOverrides{}
-	cconfig := clientcmd.NewDefaultClientConfig(*kconfig, overrides)
-	config, err := cconfig.ClientConfig()
-	if err != nil {
-		panic(err.Error())
+		log.Printf("Error loading kubectl config: [%s]", err)
+		os.Exit(1)
 	}
 
-	// create the clientset
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
+	if len(contexts) == 1 && contexts[0] == "" {
+		contexts[0] = kconfig.CurrentContext
 	}
 
-	ns, _, err := cconfig.Namespace()
-	pods, err := clientset.CoreV1().Pods(ns).List(metav1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
+	funcMap := template.FuncMap{
+		"nl":        func() string { return "\n" },
+		"t":         func() string { return "\t" },
+		"tableflip": func() string { return "(╯°□°）╯︵ ┻━┻" },
 	}
-	fmt.Printf("There are %d pods in namespace %s\n", len(pods.Items), ns)
+	tmpl, err := template.New("test").Funcs(funcMap).Parse(*format)
+	if err != nil {
+		log.Printf("Error parsing template [%s]: [%v]", *format, err)
+		os.Exit(1)
+	}
 
-	/*
-		namespaces, err := clientset.CoreV1().Namespaces().List(metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		for _, ns := range namespaces.Items {
-			pods, err := clientset.CoreV1().Pods(ns.Name).List(metav1.ListOptions{})
-			if err != nil {
-				panic(err.Error())
-			}
-			fmt.Printf("There are %d pods in namespace %s\n", len(pods.Items), ns.Name)
-		}
-	*/
 	for _, context := range contexts {
+		c, ok := kconfig.Contexts[context]
+		if !ok {
+			log.Printf("Error: context [%s] does not exist", context)
+			os.Exit(1)
+		}
 		if len(contexts) > 1 || *verbose {
-			log.Printf("context: %s", context)
+			log.Printf("Context: %s, Namespace: %s", context, c.Namespace)
 		}
 
-		e, err := getpods(context, *verbose, fs.Args())
+		e, err := getpods(kconfig, context, c.Namespace, *verbose, *labelSelector, tmpl, fs.Args())
 		if err != nil {
 			log.Printf("Error: %s", err)
 			os.Exit(e)
@@ -87,16 +79,34 @@ func main() {
 	}
 }
 
-func getpods(context string, verbose bool, args []string) (int, error) {
-	kcArgs := []string{"get", "pod"}
-	kcArgs = append(kcArgs, args...)
-	cmd := kc.PrepKC(context, kcArgs...)
-	return kc.Run(cmd, verbose)
+func getClientset(kconfig *api.Config, context string) (*kubernetes.Clientset, error) {
+	overrides := &clientcmd.ConfigOverrides{CurrentContext: context}
+	cconfig := clientcmd.NewDefaultClientConfig(*kconfig, overrides)
+	config, err := cconfig.ClientConfig()
+	if err != nil {
+		return nil, err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return clientset, nil
 }
 
-func homeDir() string {
-	if h := os.Getenv("HOME"); h != "" {
-		return h
+func getpods(kconfig *api.Config, context string, namespace string, verbose bool, labelSelector string, tmpl *template.Template, args []string) (int, error) {
+	clientset, err := getClientset(kconfig, context)
+	pods, err := clientset.CoreV1().Pods(namespace).List(metav1.ListOptions{LabelSelector: labelSelector})
+	if err != nil {
+		return 1, err
 	}
-	return os.Getenv("USERPROFILE") // windows
+	if verbose {
+		log.Printf("There are %d pods in namespace %s", len(pods.Items), namespace)
+	}
+	for _, pod := range pods.Items {
+		err = tmpl.Execute(os.Stderr, pod)
+		if err != nil {
+			return 1, err
+		}
+	}
+	return 0, nil
 }
